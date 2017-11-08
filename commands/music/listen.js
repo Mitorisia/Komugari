c9b0ed007d3cf97285feedf51f583e6bc3a5649e
Unknown <hamwlgon.ovo@gmail.com>
2017-11-08 14:31:03 -0500

    image reference in howto

--- commands/music/listen.js
+++ commands/music/listen.js
@@ -37,33 +37,33 @@ module.exports = class ListenCommand extends Command {
                     let inAC = setInterval(function() { inactivityDetectionFn(this, message) }, 10000);
                     var broadcast = this.client.createVoiceBroadcast();
 
-                    getStream() 
-                    .then(res => { 
-                        message.member.voiceChannel.join().then(connection => { 
-                            broadcast.playStream(res); 
-                            var dispatcher = connection.playBroadcast(broadcast); 
-     
-                            dispatcher.on('end', async (reason) => { 
-                            dispatcher.on('end', async(reason) => { 
-                                clearInterval(inAC); 
-                                if(reason == "heck") { 
-                                    message.channel.send("⚠ Left voice channel due to inactivity or being muted! ...Please don't abuse me!")   
-                                if (reason == "heck") { 
-                                    message.channel.send("⚠ Left voice channel due to inactivity or being muted! ...Please don't abuse me!") 
-                                    delete connection.channel.textChannel; 
-                                    return connection.disconnect(); 
-                                }   
-                                } 
-                            }); 
-                        }) 
-                    }) 
-                    .catch(err => { 
-                        console.log(err) 
-                    }); 
-                        }).catch(err => { 
-                            console.log(err) 
-                        }); 
-     
+                    getStream()
+                    .then(res => {
+                        message.member.voiceChannel.join().then(connection => {
+                            broadcast.playStream(res);
+                            var dispatcher = connection.playBroadcast(broadcast);
+
+                            dispatcher.on('end', async (reason) => {
+                            dispatcher.on('end', async(reason) => {
+                                clearInterval(inAC);
+                                if(reason == "heck") {
+                                    message.channel.send("⚠ Left voice channel due to inactivity or being muted! ...Please don't abuse me!")
+                                if (reason == "heck") {
+                                    message.channel.send("⚠ Left voice channel due to inactivity or being muted! ...Please don't abuse me!")
+                                    delete connection.channel.textChannel;
+                                    return connection.disconnect();
+                                }
+                                }
+                            });
+                        })
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    });
+                        }).catch(err => {
+                            console.log(err)
+                        });
+
 
                     message.channel.send(message.guild.me.mute ? `⚠ **${message.author.username}**, I can't play if I'm muted! Please unmute me as soon as possible!` : `🎵 Now streaming <https://listen.moe> in **${voiceChannel.name}**!`)
 
@@ -71,7 +71,7 @@ module.exports = class ListenCommand extends Command {
                     message.react('‼');
 
 
-                    
+
                     return message.channel.send('Hold on... I\'m already playing in a voice channel!');
                 }
 
