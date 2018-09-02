3ba43847d011aafcac4d6f4fb32ed376835c0475
Mitorisia <hamwlgon.ovo@gmail.com>
2018-09-02 10:39:39 -0400

    heroku frick

--- commands/fun/_talk.js
+++ commands/fun/_talk.js
@@ -1,4 +1,4 @@
-const { Command } = require('../../commando');
+/*const { Command } = require('../../commando');
 const cleverbot = require("cleverbot-node");
 
 module.exports = class TalkCommand extends Command {
@@ -34,3 +34,4 @@ module.exports = class TalkCommand extends Command {
         });
     }
 }
+*/
